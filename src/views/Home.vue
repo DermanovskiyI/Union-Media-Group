5b62cfe44cc21cf0abde5e6a509156c99fdc5eb0
dermych <igorderman@gmail.com>
2021-05-12 20:49:36 +0300

    added selection functional

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="wrapper">
-        <div class="user">{{selectUser}}</div>
+        <div class="user">
+            <div class="user__selected" v-if="selectedUser">
+                <div class="user__name">{{selectedUser.name}}</div>
+                <div class="user__id">{{selectedUser.id}}</div>
+            </div>
+            <ul class="user__not-selected">
+                <li class="user__not-selected-item" v-for="user in notSelectedUsers" :key="user.id">
+                    <div class="user__name">{{user.name}}</div>
+                    <div class="user__id">{{user.id}}</div>
+                </li>
+            </ul>
+        </div>
         <user-list
-        :users="user"
-        @handleSelect="handleSelect"
+            :users="user"
+            @handleSelect="handleSelect"
         />
     </div>
 </template>
@@ -27,6 +38,18 @@ export default {
       selectUser: 0, // id выбранного пользователя. 0 если не выбрано
     };
   },
+  computed: {
+    selectedUser() {
+      if (this.selectUser === 0) {
+        return null;
+      }
+      const filteredUsers = this.user.filter((user) => user.id === this.selectUser);
+      return filteredUsers[0];
+    },
+    notSelectedUsers() {
+      return this.user.filter((user) => user.id !== this.selectUser);
+    },
+  },
   methods: {
     handleSelect(userId) {
       if (this.selectUser === userId) {
@@ -43,5 +66,8 @@ export default {
 .user {
     margin-bottom: 20px;
     text-align: center;
+}
+.user__not-selected {
+    list-style-type: none;
 }
 </style>
